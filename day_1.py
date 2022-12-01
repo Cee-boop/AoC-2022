7cc85443e476fef913b38a4d80662882abe4f2bf
Chantelle <79821347+Cee-boop@users.noreply.github.com>
2022-12-01 16:54:11 +1100

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,14 +1,7 @@
 with open(file='data.txt') as file:
     data = file.read().split("\n\n")
 
-most_calories = 0
-all_calorie_totals = []
-for i, entry in enumerate(data):
-    curr_calories = sum(map(int, entry.split("\n")))
-    all_calorie_totals.append(curr_calories)
-    most_calories = max(most_calories, curr_calories)
-
 # part one:
-print(most_calories)
+print(max([sum(map(int, entry.split("\n"))) for entry in data]))
 # part two:
-print(sum(sorted(all_calories_totals)[-3:]))
+print(sum(sorted([sum(map(int, entry.split("\n"))) for entry in data])[-3:]))
