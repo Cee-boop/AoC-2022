72c0911e09e45d556a19f1340694b9f180e7e9a7
Chantelle <79821347+Cee-boop@users.noreply.github.com>
2022-12-07 09:57:48 +1100

    refactoring, comment changes

--- day06.py
+++ day06.py
@@ -2,22 +2,22 @@ with open(file='data.txt') as file:
     data = file.read()
 
 
-def detect_packet_marker(packet_window: int, datastream_buffer: str) -> int:
+def detect_first_marker(window_length: int, datastream_buffer: str) -> int:
     marker_index, start = 0, 0
     # use sliding window to traverse string
     for end, character in enumerate(datastream_buffer):
-        if end - start + 1 == packet_window:
-            if len(set(datastream_buffer[start:end+1])) == packet_window:  # packet found
+        if end - start + 1 == window_length:
+            if len(set(datastream_buffer[start:end+1])) == window_length:  # packet, or message found
                 marker_index = end + 1
                 return marker_index
 
             start += 1
 
-    # if no packet found
+    # if no marker found
     return 0
 
 
 # part one:
-print(detect_packet_marker(4, data))
+print(detect_first_marker(4, data))
 # part two:
-print(detect_packet_marker(14, data))
+print(detect_first_marker(14, data))
